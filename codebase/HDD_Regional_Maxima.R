0ccc47415404617f3e365aeb4e7a5147ddb8ea5e
yashamonkar <39978782+yashamonkar@users.noreply.github.com>
2022-04-13 16:38:54 -0400

    Regional Maxima

--- codebase/HDD_Regional_Maxima.R
+++ codebase/HDD_Regional_Maxima.R
@@ -1,5 +1,5 @@
 #______________________________________________________________________________#
-####Objective :- To generate AM CDD Time Series (and Save) for each Sub-Region####
+####Objective :- To generate AM HDD Time Series (and Save) for each Sub-Region####
 
 
 ###This is done since reading and opening the ERA-5 data is a long process.
@@ -65,7 +65,7 @@ all_grids <- nerc_pop_temp$Temperature
 population <- sub_region_pop_grids  #nerc_pop_temp$Population[[sel_rto]]
 
 thresh_temp <- 291.5 #-----65 Fahrenheit 
-yrs <- 1950:2021
+yrs <- 1951:2020
 block_sizes <- c(6,12,24,72, 168, 336) #hours
 n_regions <- length(nerc_sf$FID)
 nerc_labels <- nerc_sf$NERC_Label
@@ -73,12 +73,12 @@ nerc_labels <- nerc_sf$NERC_Label
 
 ###---Select Population Year---### 
 #Based on Column in Population -- 3-2000, 4-2005, 5-2010, 6-2015, 7-2020
-scenario <- 3
+scenario <- 7
 
 
 #______________________________________________________________________________#
 ###----Code to get regional block Maxima----###
-NERC_CDD_Region <- list()
+NERC_HDD_Region <- list()
 
 for(rto in 1:n_regions){
   
@@ -90,7 +90,7 @@ for(rto in 1:n_regions){
   grid_locs <- all_grids[[rto]]
   
   #Set-up Storage
-  CDD_Grid_Values <- CDD_Dates <- CDD_Site_Values <- list()
+  HDD_Grid_Values <- HDD_Dates <- HDD_Site_Values <- list()
   
   pb = txtProgressBar(min = 1, max = length(yrs), initial = 1) 
   for(y in 1:length(yrs)){
@@ -100,7 +100,40 @@ for(rto in 1:n_regions){
     #Current year
     yr <- yrs[y]
     
-    #----------------------------------------------------------------------------#
+    #Get the number of hours from Jan-01 to June 30
+    st_date <- as.POSIXct(paste0("01-01-",yr," 00:00"), format="%m-%d-%Y %H:%M")
+    yr_end <- as.POSIXct(paste0("06-30-",yr," 23:00"), format="%m-%d-%Y %H:%M")
+    yr_start <- as.POSIXct(paste0("07-01-",yr-1," 00:00"), format="%m-%d-%Y %H:%M")
+    hrs_ahead <- difftime(yr_end,st_date, units = "hours")
+    hrs_behind <- difftime(st_date,yr_start, units = "hours")
+    
+    #-------------------------------------------------------#
+    #Read a single ERA-5 download file for previous year
+    name <- paste0("data/raw_data/era_5/",yr-1,".nc")
+    nc_data <- nc_open(name)
+    
+    #Get the attributes
+    lon <- ncvar_get(nc_data, "longitude")
+    lat <- ncvar_get(nc_data, "latitude")
+    t2m <- ncvar_get(nc_data, "t2m") 
+    t2m <- t2m[,,(dim(t2m)[3]-hrs_behind):dim(t2m)[3]]
+    
+    #Subset to needed values
+    t2m_lag <- matrix(ncol = nrow(grid_locs),
+                      nrow = dim(t2m)[3])
+    
+    for(j in 1:ncol(t2m_lag)){
+      lon_gr <- which(lon == grid_locs$Longitude[j])
+      lat_gr <- which(lat == grid_locs$Latitude[j])
+      
+      t2m_lag[,j] <- t2m[lon_gr,lat_gr,]
+    }
+    
+    #Clear the .nc file
+    t2m <- NULL
+    nc_close(nc_data) #Closing the Netcdf file. 
+    
+    #-------------------------------------------------------#
     #Read a single ERA-5 download file
     name <- paste0("data/raw_data/era_5/",yr,".nc")
     nc_data <- nc_open(name)
@@ -109,33 +142,37 @@ for(rto in 1:n_regions){
     lon <- ncvar_get(nc_data, "longitude")
     lat <- ncvar_get(nc_data, "latitude")
     t2m <- ncvar_get(nc_data, "t2m") 
+    t2m <- t2m[,,1:hrs_ahead]
     
     #Subset to needed values
-    t2m_land <- matrix(ncol = nrow(grid_locs),
-                       nrow = dim(t2m)[3])
+    t2m_front <- matrix(ncol = nrow(grid_locs),
+                        nrow = dim(t2m)[3])
     
-    for(j in 1:ncol(t2m_land)){
+    for(j in 1:ncol(t2m_front)){
       lon_gr <- which(lon == grid_locs$Longitude[j])
       lat_gr <- which(lat == grid_locs$Latitude[j])
       
-      t2m_land[,j] <- t2m[lon_gr,lat_gr,]
+      t2m_front[,j] <- t2m[lon_gr,lat_gr,]
     }
     
     #Clear the .nc file
     t2m <- NULL
     nc_close(nc_data) #Closing the Netcdf file. 
     
+    #Combining the values
+    t2m_land <- rbind(t2m_lag,t2m_front)
+    t2m_lag <- t2m_front <- NULL 
     
-    #----------------------------------------------------------------------------#
-    ###Compute the Ann-Max CDD for the year
-    CDD <-   t2m_land - thresh_temp
-    CDD[CDD<0] <- 0
+    
+    #--------------------------------------------------------------------------#
+    ###Compute the Ann-Max HDD for the year
+    HDD <-  thresh_temp - t2m_land
+    HDD[HDD<0] <- 0
     
     #Multiply by Population Density Weights
-    CDD <- sweep(CDD, MARGIN=2, population_wts , `*`)
+    HDD <- sweep(HDD, MARGIN=2, population_wts , `*`)
     
-    CDD_Agg <- rowSums(CDD)
-    
+    HDD_Agg <- rowSums(HDD)
     max_val <- max_dates <- max_site_vals <-  list()
     
     for(k in 1:length(block_sizes)) {
@@ -146,34 +183,35 @@ for(rto in 1:n_regions){
       #Dates
       st_date <- paste0("01-01-",yr, " 00:00")
       start_date <- as.POSIXlt(st_date, format="%m-%d-%Y %H:%M")
-      time_stamps <- seq(start_date, by = "hour", length.out = length(CDD_Agg))
+      time_stamps <- seq(start_date, by = "hour", length.out = length(HDD_Agg))
       
       #Compute the moving average
-      df <- data.frame(Metric = CDD_Agg, 
+      df <- data.frame(Metric = HDD_Agg, 
                        Dates = time_stamps, 
-                       Index = 1:length(CDD_Agg))
+                       Index = 1:length(HDD_Agg))
       df$Block <- rollsum(df$Metric, cur_block, align = "center", fill = NA)
       df$Metric <- NULL
       df[is.na(df)] <- 0
       max_index <- which(df$Block==max(df$Block))
       
       #Get the mean values across the grid
-      max_dates[[k]] <- df$Dates[max_index]
+      max_dates[[k]] <- df$Dates[max_index]- hrs_behind
       max_val[[k]] <- df$Block[max_index]
       
       #Get the Site values
-      #max_site_vals[[k]] <- CDD[max_index,] #Just the middle hour
+      #max_site_vals[[k]] <- HDD[max_index,] #Just the middle hour
       low_index <- max_index - cur_block*0.5 + 1
       upper_index <- max_index + cur_block*0.5
-      temp_sites <- CDD[low_index:upper_index,]
+      temp_sites <- HDD[low_index:upper_index,]
       max_site_vals[[k]] <- colSums(temp_sites)
       
     }
-    CDD <- NULL
+    HDD <- NULL
     
-    CDD_Grid_Values[[y]] <- max_val
-    CDD_Dates[[y]] <- max_dates
-    CDD_Site_Values[[y]] <-  max_site_vals
+    HDD_Grid_Values[[y]] <- max_val
+    HDD_Dates[[y]] <- max_dates
+    HDD_Site_Values[[y]] <-  max_site_vals
+    
   }
   
   
@@ -184,41 +222,42 @@ for(rto in 1:n_regions){
     #Getting the Grid Values
     temp <- list()
     for(i in 1:length(yrs)){
-      temp[[i]] <- CDD_Grid_Values[[i]][[j]]
+      temp[[i]] <- HDD_Grid_Values[[i]][[j]]
     }
     Grid_Values[[j]] <- unlist(temp)
     
     #Getting the Grid Dates
     temp <- seq(1:length(yrs))
     for(i in 1:length(yrs)){
-      temp[i] <- as.character(CDD_Dates[[i]][[j]])
+      temp[i] <- as.character(HDD_Dates[[i]][[j]])
     }
     Grid_Dates[[j]] <- temp
     
     #Getting the Site Values
     temp <- matrix(NA, ncol = nrow(grid_locs), nrow = length(yrs))
     for(i in 1:length(yrs)){
-      temp[i,] <- CDD_Site_Values[[i]][[j]]
+      temp[i,] <- HDD_Site_Values[[i]][[j]]
     }
     Site_Values[[j]] <- temp
   }
   
+  
   #Creating a Single List
-  CDD_Regional <- list()
-  CDD_Regional[[1]] <- Grid_Values
-  CDD_Regional[[2]] <- Grid_Dates
-  CDD_Regional[[3]] <- Site_Values
+  HDD_Regional <- list()
+  HDD_Regional[[1]] <- Grid_Values
+  HDD_Regional[[2]] <- Grid_Dates
+  HDD_Regional[[3]] <- Site_Values
   
   #Plots - Consistency Checks
-  plot(yrs, CDD_Regional[[1]][[1]], type='l',
+  plot(yrs, HDD_Regional[[1]][[1]], type='l',
        xlab = "Year", ylab = "Degree-Hours/Ann Max Event", 
-       main = " Annual Maximum CDD across CONUS for 6 hr events")
+       main = " Annual Maximum HDD across CONUS for 6 hr events")
   mtext(paste0(nerc_labels[rto]), side = 3,
         cex = 1.15)
   
   #Spatial Plots
   sub_region <- nerc_sf[rto,]
-  pt_dt <- CDD_Regional[[3]][[1]]
+  pt_dt <- HDD_Regional[[3]][[1]]
   pt_dt <- colMeans(pt_dt)
   
   p1 <- ggplot() +
@@ -234,13 +273,13 @@ for(rto in 1:n_regions){
                                     fill = pt_dt)) +
     scale_fill_gradient2(midpoint=median(pt_dt),
                          low="blue", mid="white",high="red",
-                         name = "CDD") +
+                         name = "HDD") +
     ggtitle("Grid Maximum  - Block Size 6 Hours - Code Consistency Check")
   #print(p1)
   
   
   ###Saving 
-  NERC_CDD_Region[[rto]] <- CDD_Regional
+  NERC_HDD_Region[[rto]] <- HDD_Regional
   
   
   
@@ -249,4 +288,5 @@ for(rto in 1:n_regions){
 
 #______________________________________________________________________________#
 ##Saving the results
-save(NERC_CDD_Region, file = paste0("data/processed_data/CDD_Regional_2000.RData"))
+save(NERC_HDD_Region, file = paste0("data/processed_data/HDD_Regional_2020.RData"))
+
